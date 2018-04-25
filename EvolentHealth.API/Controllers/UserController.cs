e6b60455b697e298972a305501e3bfd3230e36c1
Hemant Singh Negi <38699571+hemantsingh22jan@users.noreply.github.com>
2018-04-25 23:10:27 +0530

    Web API
    Web API committed

---
+++ EvolentHealth.API/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using EvolentHeath.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EvolentHealth.API.Controllers
+{
+    public class UserController : ApiController
+    {
+        EvolentContext context = new EvolentContext();
+
+        [HttpPost]
+        public int PostAddUser([FromBody] User user)
+        {
+
+            User obj = new User();
+            if (user != null)
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+
+                return user.UserId;
+            }
+            else
+                return 0;
+        }
+
+        [HttpGet]
+        public User GetUser(int id)
+        {
+            User user = context.Users.Find(id);
+            return user;
+        }
+
+        [HttpGet]
+        public List<User> GetUsersList()
+        {
+
+            return context.Users.ToList();
+        }
+
+        [HttpPut]
+        public IHttpActionResult PutModifyUser([FromBody]User user)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("User details are not valid");
+            using (var ctx = new EvolentContext())
+            {
+                var existingUser = ctx.Users.Where(rec => rec.UserId == user.UserId)
+                                   .FirstOrDefault<User>();
+
+                if (existingUser != null)
+                {
+                    existingUser.Name = user.Name;
+                    existingUser.Address = user.Address;
+                    existingUser.ContactNo = user.ContactNo;
+
+                    ctx.Entry(existingUser).State = EntityState.Modified;
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        [HttpDelete]
+        public void DeleteUser(int id)
+        {
+            User user = context.Users.Find(id);
+            if (user != null)
+            {
+                context.Users.Remove(user);
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
